b72d2f2eb50ec08cfdeb0da5164005048fb61ff3
So-Andrey <115418958+So-Andrey@users.noreply.github.com>
2023-02-28 12:00:47 +0300

    Commit

--- src/allForDragons/DragonsCollection.java
+++ src/allForDragons/DragonsCollection.java
@@ -26,43 +26,45 @@ public class DragonsCollection {
         boolean scan = true;
         while (scan) {
 
-            String[] dragon = scanner.nextLine().split(", ");
-            if (dragon.length == 8) {
-                try {
-                    if (!dragon[0].trim().isEmpty()) {
-                        name = dragon[0];
-                    } else {
-                        throw new InputMismatchException();
-                    }
-                    age = Long.parseLong(dragon[1]);
-                    x = Long.parseLong(dragon[2]);
-                    y = Float.parseFloat(dragon[3]);
-                    if (dragon[4].equals("GREEN")||dragon[4].equals("ORANGE")||dragon[4].equals("BROWN")||dragon[4].equals("null")|dragon[5].equals("WATER")||dragon[5].equals("UNDERGROUND")||dragon[5].equals("FIRE")||dragon[6].equals("CUNNING")||dragon[6].equals("CHAOTIC_EVIL")||dragon[6].equals("WISE")||dragon[6].equals("FICKLE")) {
-                        switch (dragon[4]) {
-                            case "GREEN" -> color = Color.GREEN;
-                            case "ORANGE" -> color = Color.ORANGE;
-                            case "BROWN" -> color = Color.BROWN;
-                            case "null" -> color = null;
+            try {
+                String[] dragon = scanner.nextLine().split(", ");
+                if (dragon.length == 8) {
+                    try {
+                        if (!dragon[0].trim().isEmpty()) {
+                            name = dragon[0];
+                        } else {
+                            throw new InputMismatchException();
                         }
-                        switch (dragon[5]) {
-                            case "WATER" -> type = DragonType.WATER;
-                            case "UNDERGROUND" -> type = DragonType.UNDERGROUND;
-                            case "FIRE" -> type = DragonType.FIRE;
+                        age = Long.parseLong(dragon[1]);
+                        x = Long.parseLong(dragon[2]);
+                        y = Float.parseFloat(dragon[3]);
+                        if (dragon[4].equals("GREEN")||dragon[4].equals("ORANGE")||dragon[4].equals("BROWN")||dragon[4].equals("null")|dragon[5].equals("WATER")||dragon[5].equals("UNDERGROUND")||dragon[5].equals("FIRE")||dragon[6].equals("CUNNING")||dragon[6].equals("CHAOTIC_EVIL")||dragon[6].equals("WISE")||dragon[6].equals("FICKLE")) {
+                            switch (dragon[4]) {
+                                case "GREEN" -> color = Color.GREEN;
+                                case "ORANGE" -> color = Color.ORANGE;
+                                case "BROWN" -> color = Color.BROWN;
+                                case "null" -> color = null;
+                            }
+                            switch (dragon[5]) {
+                                case "WATER" -> type = DragonType.WATER;
+                                case "UNDERGROUND" -> type = DragonType.UNDERGROUND;
+                                case "FIRE" -> type = DragonType.FIRE;
+                            }
+                            switch (dragon[6]) {
+                                case "CUNNING" -> character = DragonCharacter.CUNNING;
+                                case "WISE" -> character = DragonCharacter.WISE;
+                                case "CHAOTIC_EVIL" -> character = DragonCharacter.CHAOTIC_EVIL;
+                                case "FICKLE" -> character = DragonCharacter.FICKLE;
+                            }
+                        } else {
+                            throw new InputMismatchException();
                         }
-                        switch (dragon[6]) {
-                            case "CUNNING" -> character = DragonCharacter.CUNNING;
-                            case "WISE" -> character = DragonCharacter.WISE;
-                            case "CHAOTIC_EVIL" -> character = DragonCharacter.CHAOTIC_EVIL;
-                            case "FICKLE" -> character = DragonCharacter.FICKLE;
-                        }
-                    } else {
-                        throw new InputMismatchException();
-                    }
-                    eyesCount = Double.parseDouble(dragon[7]);
-                    dragons.add(new Dragon(name, new Coordinates(x, y), age, color, type, character, new DragonHead(eyesCount)));
-                } catch (Exception ignored) {}
-            }
-            scan = scanner.hasNext();
+                        eyesCount = Double.parseDouble(dragon[7]);
+                        dragons.add(new Dragon(name, new Coordinates(x, y), age, color, type, character, new DragonHead(eyesCount)));
+                    } catch (Exception ignored) {}
+                }
+                scan = scanner.hasNext();
+            } catch (NoSuchElementException ignore) {}
         }
     }
 }
