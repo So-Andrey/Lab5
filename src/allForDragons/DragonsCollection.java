4081b120d23cd4ad57362f2db4a0c74bcedbe64f
So-Andrey <115418958+So-Andrey@users.noreply.github.com>
2023-03-09 18:21:49 +0300

    issue 7 done

--- src/allForDragons/DragonsCollection.java
+++ src/allForDragons/DragonsCollection.java
@@ -21,43 +21,48 @@ public class DragonsCollection {
                 "Количество элементов: " + dragons.size() + "\n");
     }
     /**Метод для добавления драконов из файла в коллекцию*/
-    public static void putDragonsFromFile() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File(Invoker.getFile()));
-        String name;
-        long x;
-        float y;
-        long age;
-        Color color;
-        DragonType type;
-        DragonCharacter character;
-        double eyesCount;
-        while (scanner.hasNext()) {
-            try {
-                String[] dragon = scanner.nextLine().split(", ");
-                if (dragon.length == 8) {
-                    try {
-                        if (!dragon[0].trim().isEmpty()) {
-                            name = dragon[0];
-                        } else {
-                            throw new InputMismatchException();
-                        }
-                        age = Long.parseLong(dragon[1]);
-                        x = Long.parseLong(dragon[2]);
-                        y = Float.parseFloat(dragon[3]);
-                        if (dragon[4].equals("GREEN") || dragon[4].equals("ORANGE") || dragon[4].equals("BROWN") || dragon[4].equals("null") | dragon[5].equals("WATER") || dragon[5].equals("UNDERGROUND") || dragon[5].equals("FIRE") || dragon[6].equals("CUNNING") || dragon[6].equals("CHAOTIC_EVIL") || dragon[6].equals("WISE") || dragon[6].equals("FICKLE")) {
-                            color = Color.valueOf(dragon[4]);
-                            type = DragonType.valueOf(dragon[5]);
-                            character = DragonCharacter.valueOf(dragon[6]);
-                        } else {
-                            throw new InputMismatchException();
-                        }
-                        eyesCount = Double.parseDouble(dragon[7]);
-                        dragons.add(new Dragon(name, new Coordinates(x, y), age, color, type, character, new DragonHead(eyesCount)));
-                    } catch (Exception ignored) {}
-                }
-            } catch (Exception ignored) {}
+    public static void putDragonsFromFile() {
+        try {
+            Scanner scanner = new Scanner(new File(Invoker.getFile()));
+            String name;
+            long x;
+            float y;
+            long age;
+            Color color;
+            DragonType type;
+            DragonCharacter character;
+            double eyesCount;
+            while (scanner.hasNext()) {
+                try {
+                    String[] dragon = scanner.nextLine().split(", ");
+                    if (dragon.length == 8) {
+                        try {
+                            if (!dragon[0].trim().isEmpty()) {
+                                name = dragon[0];
+                            } else {
+                                throw new InputMismatchException();
+                            }
+                            age = Long.parseLong(dragon[1]);
+                            x = Long.parseLong(dragon[2]);
+                            y = Float.parseFloat(dragon[3]);
+                            if (dragon[4].equals("GREEN") || dragon[4].equals("ORANGE") || dragon[4].equals("BROWN") || dragon[4].equals("null") | dragon[5].equals("WATER") || dragon[5].equals("UNDERGROUND") || dragon[5].equals("FIRE") || dragon[6].equals("CUNNING") || dragon[6].equals("CHAOTIC_EVIL") || dragon[6].equals("WISE") || dragon[6].equals("FICKLE")) {
+                                color = Color.valueOf(dragon[4]);
+                                type = DragonType.valueOf(dragon[5]);
+                                character = DragonCharacter.valueOf(dragon[6]);
+                            } else {
+                                throw new InputMismatchException();
+                            }
+                            eyesCount = Double.parseDouble(dragon[7]);
+                            dragons.add(new Dragon(name, new Coordinates(x, y), age, color, type, character, new DragonHead(eyesCount)));
+                        } catch (Exception ignored) {}
+                    }
+                } catch (Exception ignored) {}
+            }
+            scanner.close();
+            System.out.println("Из файла добавлено объектов в коллекцию: " + dragons.size());
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Файл не найден");
         }
-        scanner.close();
-        System.out.println("Из файла добавлено объектов в коллекцию: " + dragons.size());
+
     }
 }
